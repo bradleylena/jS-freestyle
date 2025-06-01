c86621d4e44f3d7039b9e220a279db033a596b08
bradleylena <kabakabrad@gmail.com>
2025-06-02 01:39:33 +0300

    learning

---
+++ script.js
@@ -0,0 +1,22 @@
+// alert("hoodbyair");
+// alert("hello");
+document.getElementById("rada").innerText = "goog";
+function myfunction() {
+  document.getElementById("h1para").innerHTML = "hello UNIVERSE";
+}
+function ourfunction() {
+  document.getElementById("h3para").innerHTML = "Goodbye";
+}
+function h4function() {
+  document.getElementById("h4para").innerHTML = "LENAIYARRA";
+}
+function radafunction() {
+  document.getElementById("rada").innerHTML = "<h3>Goodbye</h3>";
+}
+function readyfunction() {
+  alert("closing soon");
+  document.getElementById("alert").innerHTML = "YOU HAVE ACTIVATED";
+}
+function function1() {
+  window.alert("CLICKED AGAIN?");
+}
