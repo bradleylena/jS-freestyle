0270cab4deec3ad76e369459831d51ff6519cac3
bradleylena <kabakabrad@gmail.com>
2025-06-04 02:06:49 +0300

    more js

--- script.js
+++ script.js
@@ -24,8 +24,6 @@ function func3() {
   document.writeln(5 + 2);
 }
 
-document.getElementById("class7").innerHTML = "The value of z is " + z + ".";
-
 let x, y;
 x = 5;
 y = 3;
@@ -36,4 +34,69 @@ console.log(5 + 6);
 function inac() {
   alert("this site is now active");
   document.getElementById("inac").innerHTML = "now rolling";
+}
+let lastname, lastName;
+lastname = "brad";
+lastName = "Lena";
+
+let f = 5;
+let k = 20;
+let r = f + k;
+
+function ghost() {
+  document.getElementById("ghost").innerHTML = "no more ghost";
+  console.log(r);
+}
+const pi = 3.14;
+console.log(pi);
+function bmw() {
+  let carname = "Mercedes";
+  document.getElementById("bmw").innerHTML = carname;
+}
+document.getElementById("rada").innerHTML = "audi";
+// Select the toggle button
+const toggleButton = document.getElementById("toggle-dark-mode");
+
+// Check for saved theme in localStorage
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+// Toggle dark mode on button click
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Save theme in localStorage
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+const PI = 3.14;
+
+function functioncars() {
+  const cars = ["merc", "bmw", "poad"];
+  cars[2] = "gwagon";
+  cars[3] = "rolls";
+
+  cars.push("panamera");
+  document.getElementById("cars").innerHTML = cars;
+}
+
+const coupes = { type: "3series", model: "m340i", color: "black" };
+document.getElementById("bmwseries").innerHTML =
+  "type of car is " + coupes.model;
+
+function series() {
+  document.getElementById("bmwseries").innerHTML =
+    "type of car is " + coupes.color;
+}
+{
+  const cars = ["c-class", "e-class", "e63s"];
+  cars[0] = "g-class";
+  cars.push("g63");
+  console.log(cars);
+  document.getElementById("ourseries").innerHTML = cars;
+  alert(cars);
 }
